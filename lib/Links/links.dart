2b5a6ec58110fa7a7e83bf1797a8fd977ac0ad56
oolaa <oolaa.pleur@gmail.com>
2023-06-13 19:39:01 +0300

    add links to commit

---
+++ lib/Links/links.dart
@@ -0,0 +1,5 @@
+class Links {
+  static const String TARTU_BIKES_LINK = 'https://api.ratas.tartu.ee/cxf/am/';
+  static const String BOLT_SCOOTERS_LINK = 'https://user.live.boltsvc.net/micromobility/search/getVehicles';
+  static const BOLT_HEADER = {'Authorization': 'Basic KzM3MjUzMjY5NjIyOjQxNTlhZWE3LTBlMjEtNGI2Mi05ZmQ1LTg0MmM1NjdhZWRhOQ=='};
+}
