9e873e2cdb707be2fb2aec0dbdfc753e6bb4c358
OolaaPleur <oolaa.pleur@gmail.com>
2025-03-01 18:00:47 +0000

    Deploying to gh-pages from @ OolaaPleur/Yggert-nu@9f5c51447e01e6ec57a4c1f8b32bb5c70a9858ca 🚀

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1074074169"
+    serviceWorkerVersion: "239112352"
   }
 });
